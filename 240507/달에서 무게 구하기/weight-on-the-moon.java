f5f46c7f4482b965c79629f49d5f9ab2d82ed731
Lee youngchan <lych0918@gachon.ac.kr>
2024-05-07 22:46:43 +0900

    Title: 달에서 무게 구하기, Time: 102ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240507/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int weight = 13;
+        float g = 0.165f;
+
+        System.out.printf("%d * %.6f = %.6f", weight, g, weight * g);
+    }
+}
