d7621fd364f76e6d858decec2f50b14673fe92c1
Lee youngchan <lych0918@gachon.ac.kr>
2024-05-07 10:53:41 +0900

    Title: 다양하게 출력, Time: 96ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240507/다양하게 출력/print-in-variety.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Total days in Year");
+        System.out.println("365\nCircumference rate");
+        System.out.println(3.1415926535);
+    }
+}
