fce1a6262be18b1314a76dcd29723f73d0665791
Lee youngchan <lych0918@gachon.ac.kr>
2024-05-08 15:44:21 +0900

    Title: 입력받아 계산 4, Time: 121ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240508/입력받아 계산 4/input-calculate-4.java
@@ -0,0 +1,8 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine()) * 2;
+        System.out.print(a);
+    }
+}
