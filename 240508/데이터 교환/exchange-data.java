729e7e995261bf8130c5c2a181cc57c104717011
Lee youngchan <lych0918@gachon.ac.kr>
2024-05-08 15:33:37 +0900

    Title: 데이터 교환, Time: 95ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240508/데이터 교환/exchange-data.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+        int c = 7;
+        int tmp = c;
+        c = b;
+        b = a;
+        a = tmp;
+        System.out.println(a + "\n" + b + "\n" + c);
+    }
+}
