3d60f5bbd3edbfb7b08385192c42e5e6b62dcd38
Lee youngchan <lych0918@gachon.ac.kr>
2024-05-08 17:03:23 +0900

    Title: 입력받은 값과 합 출력, Time: 108ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240508/입력받은 값과 합 출력/output-of-value-and-sum.java
@@ -0,0 +1,10 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] in = br.readLine().split(" ");
+        int a = Integer.parseInt(in[0]);
+        int b = Integer.parseInt(in[1]);
+        System.out.print(a + " " + b + " " + (a+b));
+   }
+}
