d11d8593836cb2578264c26c6c94c6721cd1cebc
Lee youngchan <lych0918@gachon.ac.kr>
2024-05-15 16:19:29 +0900

    Title: 두수의 곱과 몫, Time: 94ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240515/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.java
@@ -0,0 +1,12 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] in = br.readLine().split(" ");
+        int a = Integer.parseInt(in[0]);
+        int b = Integer.parseInt(in[1]);
+
+        System.out.printf("%d * %d = %d\n", a, b, a*b);
+        System.out.printf("%d / %d = %d", a, b, a/b);
+    }
+}
