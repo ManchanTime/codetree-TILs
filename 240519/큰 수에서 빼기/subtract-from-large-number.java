6ed28842914f41bc909a1517be562f75889e06f3
Lee youngchan <lych0918@gachon.ac.kr>
2024-05-19 14:57:54 +0900

    Title: 큰 수에서 빼기, Time: 97ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240519/큰 수에서 빼기/subtract-from-large-number.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] in = br.readLine().split(" ");
+        Arrays.sort(in);
+        int a = Integer.parseInt(in[0]);
+        int b = Integer.parseInt(in[1]);
+        System.out.println(b-a);
+    }
+}
