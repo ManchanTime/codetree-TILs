984efb2525c1302621000848f223880f7bb51476
Lee youngchan <lych0918@gachon.ac.kr>
2024-05-21 20:43:42 +0900

    Title: 살 수 있는 물건, Time: 94ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240521/살 수 있는 물건/things-able-to-buy.java
@@ -0,0 +1,16 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] in = br.readLine().split(" ");
+        int a = Integer.parseInt(in[0]);
+        String re = "no";
+        if(a >= 3000){
+            re = "book";
+        }
+        else if(a >= 1000){
+            re = "mask";
+        }
+        System.out.println(re);
+    }
+}
