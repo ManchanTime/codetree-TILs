a21d072c1d839c17f8b564e4b59f06fe6a86d77e
Lee youngchan <lych0918@gachon.ac.kr>
2024-05-26 15:28:20 +0900

    Title: 특정 조건 두 정수 비교, Time: 89ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240526/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] in = br.readLine().split(" ");
+        int a = Integer.parseInt(in[0]);
+        int b = Integer.parseInt(in[1]);
+        int re1 = 1;
+        int re2 = 1;
+        if(a > b){
+            re1 = 0;
+        }
+        
+        if(a != b){
+            re2 = 0;
+        }
+        System.out.println(re1 + " " + re2);
+    }
+}
