4e3a1cdfe4842a03f914ee4dbd0d9cac53262646
Lee youngchan <lych0918@gachon.ac.kr>
2024-05-28 11:06:11 +0900

    Title: 비교 연산, Time: 90ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240528/비교 연산/comparison-operator.java
@@ -0,0 +1,16 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] in = br.readLine().split(" ");
+        int a = Integer.parseInt(in[0]);
+        int b = Integer.parseInt(in[1]);
+
+        System.out.println(a >= b ? 1 : 0);
+        System.out.println(a > b ? 1 : 0);
+        System.out.println(a <= b ? 1 : 0);
+        System.out.println(a < b ? 1 : 0);
+        System.out.println(a == b ? 1 : 0);
+        System.out.println(a != b ? 1 : 0);
+    }
+}
