4a14eb6e2110a952d3b72b51247e32109ebc8905
Lee youngchan <lych0918@gachon.ac.kr>
2024-06-21 23:02:59 +0900

    Title: 세 정수의 최솟값, Time: 104ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240621/세 정수의 최솟값/minimum-of-three-numbers.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] in = br.readLine().split(" ");
+        int a = Integer.parseInt(in[0]);
+        int b = Integer.parseInt(in[1]);
+        int c = Integer.parseInt(in[2]);
+        System.out.println(Math.min(a, Math.min(b, c)));
+    }
+}
