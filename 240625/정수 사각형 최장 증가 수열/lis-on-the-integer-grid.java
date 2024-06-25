29f5dda8bc6ed7591bdc522dca7769130f3e6497
Lee youngchan <lych0918@gachon.ac.kr>
2024-06-25 18:37:18 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 692ms, Memory: 38MB, Status: Passed - CodeTree

--- 240625/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.java
+++ 240625/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.java
@@ -3,61 +3,59 @@ import java.util.*;
 
 public class Main {
 
-    public static int[][] store;
+    public static int[][] DP;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int[][] map = new int[N][N];
+        DP = new int[N][N];
+        List<NodeInfo> nodeInfoList = new ArrayList<>();
         for(int i = 0; i < N; i++) {
             String[] in = br.readLine().split(" ");
             for(int j = 0; j < N; j++) {
                 map[i][j] = Integer.parseInt(in[j]);
+                nodeInfoList.add(new NodeInfo(i, j, map[i][j]));
             }
         }
-        int result = 0;
-        for(int i = 0; i < N; i++) {
-            for(int j = 0; j < N; j++) {
-                store = new int[N][N];
-                bfs(map, new NodeInfo(i, j));
-                result = Math.max(bfs(map, new NodeInfo(i, j)), result);
+        nodeInfoList.sort(new Comparator<NodeInfo>() {
+            @Override
+            public int compare(NodeInfo o1, NodeInfo o2) {
+                return o1.value - o2.value;
             }
-        }
-        System.out.println(result);
-    }
-
-    public static int bfs(int[][] map, NodeInfo start) {
-        int result = 1;
+        });
         int[] moveX = {-1, 1, 0, 0};
         int[] moveY = {0, 0, -1, 1};
-        Queue<NodeInfo> queue = new LinkedList<>();
-        queue.add(start);
-        store[start.x][start.y] = 1;
-        while(!queue.isEmpty()) {
-            NodeInfo now = queue.poll();
+        for (NodeInfo nodeInfo : nodeInfoList) {
             for(int i = 0; i < 4; i++) {
-                int x = now.x + moveX[i];
-                int y = now.y + moveY[i];
-                if(x < 0 || x >= map.length)
+                int x = nodeInfo.x + moveX[i];
+                int y = nodeInfo.y + moveY[i];
+                if(x < 0 || x >= N || y < 0 || y >= N) {
                     continue;
-                if(y < 0 || y >= map[0].length)
-                    continue;
-                if(map[x][y] > map[now.x][now.y]) {
-                    store[x][y] = store[now.x][now.y] + 1;
-                    result = Math.max(result, store[x][y]);
-                    queue.add(new NodeInfo(x, y));
+                }
+                if(nodeInfo.value < map[x][y]) {
+                    DP[x][y] = Math.max(DP[x][y], DP[nodeInfo.x][nodeInfo.y] + 1);
                 }
             }
         }
-        return result;
+
+        int result = 0;
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < N; j++) {
+                result = Math.max(result, DP[i][j]);
+            }
+        }
+        System.out.println(result + 1);
     }
 
     public static class NodeInfo {
         int x;
         int y;
+        int value;
 
-        public NodeInfo(int x, int y) {
+        public NodeInfo(int x, int y, int value) {
             this.x = x;
             this.y = y;
+            this.value = value;
         }
     }
 }
