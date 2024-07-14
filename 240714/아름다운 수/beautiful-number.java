c5a87780727b16a4026a2daadf5f91dd9f3fbf75
Lee youngchan <lych0918@gachon.ac.kr>
2024-07-14 15:12:53 +0900

    Title: 아름다운 수, Time: 100ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240714/아름다운 수/beautiful-number.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static int result;
+    public static int n;
+    public static String[] possible;
+    public static boolean[] used;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        possible = new String[]{"1", "22", "333", "4444"};
+        used = new boolean[possible.length];
+        result = 0;
+        backTracking("");
+        System.out.println(result);
+    }
+
+    public static void backTracking(String re) {
+        if(re.length() == n){
+            result++;
+            return;
+        }
+        else if(re.length() > n){
+            return;
+        }
+        for(int i = 0; i < possible.length; i++) {
+            if(!used[i]){
+                backTracking(re + possible[i]);
+            }
+        }
+    }
+}
