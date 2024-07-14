8ac777bf16d6ffdc57a4a0ec917fbced5339ec7d
Lee youngchan <lych0918@gachon.ac.kr>
2024-07-14 14:27:32 +0900

    Title: 양수 직사각형의 최대 크기, Time: 129ms, Memory: 9MB, Status: Passed - CodeTree

--- 240714/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.java
+++ 240714/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.java
@@ -3,12 +3,13 @@ import java.util.*;
 
 public class Main {
 
+    public static int[][] grid;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] in = br.readLine().split(" ");
         int n = Integer.parseInt(in[0]);
         int m = Integer.parseInt(in[1]);
-        int[][] grid = new int[n][m];
+        grid = new int[n][m];
         int result = -1;
         for(int i = 0; i < n; i++) {
             in = br.readLine().split(" ");
@@ -20,32 +21,27 @@ public class Main {
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
                 if(grid[i][j] > 0) {
-                    result = Math.max(result, getSize(grid, i, j, n - 1, m - 1));
+                    for(int x = i; x < n; x++) {
+                        for(int y = j; y < m; y++) {
+                            if(check(i, j, x, y)) {
+                                result = Math.max(result, (x - i + 1) * (y - j + 1));
+                            }
+                        }
+                    }
                 }
             }
         }
-        
         System.out.println(result);
     }
 
-    public static int getSize(int[][] grid, int startX, int startY, int endX, int endY) {
-        int result = 1;
-        boolean flag = false;
+    public static boolean check(int startX, int startY, int endX, int endY) {
         for(int i = startX; i <= endX; i++) {
             for(int j = startY; j <= endY; j++) {
-                if(grid[i][j] < 0) {
-                    if (i > 0) {
-                        result = Math.max(result, getSize(grid, startX, startY, i - 1, endY));
-                    }
-                    result = Math.max(result, getSize(grid, startX, startY, endX, j - 1));
-                    flag = true;
+                if(grid[i][j] <= 0) {
+                    return false;
                 }
             }
         }
-        if(!flag) {
-            result = (endX - startX + 1) * (endY - startY + 1);
-        }
-        //System.out.println(startX + " " + startY + " " + endX + " " + endY + " " + result);
-        return result;
+        return true;
     }
 }
