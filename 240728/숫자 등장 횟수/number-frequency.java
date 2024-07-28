657b3a4923eebea0b3f2f7d9c1a3b41805190e71
Lee youngchan <lych0918@gachon.ac.kr>
2024-07-28 17:17:42 +0900

    Title: 숫자 등장 횟수, Time: 539ms, Memory: 33MB, Status: Passed - CodeTree

--- 240728/숫자 등장 횟수/number-frequency.java
+++ 240728/숫자 등장 횟수/number-frequency.java
@@ -13,7 +13,7 @@ public class Main {
         HashMap<Integer, Integer> map = new HashMap<>();
         in = br.readLine().split(" ");
         for (int i = 0; i < n; i++) {
-            map.put(Integer.parseInt(in[i]), map.getOrDefault(Integer.parseInt(in[0]), 0) + 1);
+            map.put(Integer.parseInt(in[i]), map.getOrDefault(Integer.parseInt(in[i]), 0) + 1);
         }
         in = br.readLine().split(" ");
         for(int i = 0; i < m; i++) {
